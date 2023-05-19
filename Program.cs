63a38ea37f8e116cc9dd78915728e1bd80da0d98
ZhouSusan <szhou089@gmail.com>
2023-05-18 17:16:58 -0700

    Added else statement for the winning condition and console statemnts

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace NumberGuessingGame
             // Choose a random number between 0 and 20
             int theNumber = new Random().Next(20);
             int userGuessNumber = 0;
+            int guessCounts = 0;
 
             // Print the game greeting and instructions
             Console.WriteLine("Let's Play 'Guess the Number' human!");
@@ -17,16 +18,16 @@ namespace NumberGuessingGame
             Console.WriteLine("Enter your guess, or -1 to give up like a human, purr.");
             do
             {
+                Console.WriteLine("What's your guess?");
                 string theGuess = Console.ReadLine();
                 bool result = Int32.TryParse(theGuess, out userGuessNumber);
-                Console.WriteLine(theGuess);
+
                 if (!result)
                 {
                     Console.WriteLine("Hmmm, that doesn't look like a number. Try again or you can ask woofers over there to help.");
                 }
                 else
                 {
-                    // Keep track of the number of guesses and the current user guess
                     if (userGuessNumber == -1)
                     {
                         keepPlaying = false;
@@ -38,7 +39,13 @@ namespace NumberGuessingGame
                     else if (userGuessNumber > theNumber)
                     {
                         Console.WriteLine("Purrr, too high human~ Try again =^.^=");
+                    } else
+                    {
+                        Console.WriteLine($"You got it in {guessCounts+1} guesses!!");
+                        keepPlaying = false;
                     }
+
+                    guessCounts++;
                 }
             } while (keepPlaying);
         }
